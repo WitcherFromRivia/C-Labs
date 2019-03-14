499f47ac93fcac155a47d12777ad2b76d2188f65
WitcherFromRivia <48452428+WitcherFromRivia@users.noreply.github.com>
2019-03-14 19:37:57 +0200

    Add files via upload

---
+++ C++ Lab1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+struct Computer {
+	short size;
+	int day;
+	int month;
+	int year;
+	float prize;
+};
+int main()
+{
+	Computer AMD;
+	AMD.size = 8;
+	AMD.day = 24;
+	AMD.month = 10;
+	AMD.year = 2000;
+	AMD.prize = 8569.5;
+	cout << "Producer -"<<" AMD \n"<<"Size of RAM - "<<AMD.size<<" Gb"<<"\n"<<"Release date - "<<AMD.day<<"."<<AMD.month<<"."<< AMD.year <<"\n"<<"Min prize -"<<AMD.prize<<"$" <<endl;
+}
