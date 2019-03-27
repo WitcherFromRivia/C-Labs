2ed601e384998e7f3b34f45c45068526ebf08adf
WitcherFromRivia <48452428+WitcherFromRivia@users.noreply.github.com>
2019-03-27 20:16:06 +0200

    Update C++ Lab1.cpp

--- C++ Lab1.cpp
+++ C++ Lab1.cpp
@@ -11,11 +11,26 @@ struct Computer {
 };
 int main()
 {
-	Computer AMD;
-	AMD.size = 8;
-	AMD.day = 24;
-	AMD.month = 10;
-	AMD.year = 2000;
-	AMD.prize = 8569.5;
-	cout << "Producer -"<<" AMD \n"<<"Size of RAM - "<<AMD.size<<" Gb"<<"\n"<<"Release date - "<<AMD.day<<"."<<AMD.month<<"."<< AMD.year <<"\n"<<"Min prize -"<<AMD.prize<<"$" <<endl;
+	Computer AMD№1;
+	AMD№1.size = 8;
+	AMD№1.day = 24;
+	AMD№1.month = 10;
+	AMD№1.year = 2010;
+	AMD№1.prize = 8569.5;
+
+	Computer AMD№2;
+	AMD№2 = { 4,13,3,2009,4599 };
+
+	Computer AMD№3;
+	AMD№3 = { 6,25,8,2011,6999 };
+	if (AMD№1.prize < AMD№2.prize && AMD№1.prize < AMD№3.prize) {
+		cout << "Producer -" << " AMD \n" << "Size of RAM - " << AMD№1.size << " Gb" << "\n" << "Release date - " << AMD№1.day << "." << AMD№1.month << "." << AMD№1.year << "\n" << "Prize -" << AMD№1.prize << "$" << endl;
+	}
+	else if(AMD№2.prize < AMD№1.prize && AMD№2.prize < AMD№3.prize){
+		cout << "Producer -" << " AMD \n" << "Size of RAM - " << AMD№2.size << " Gb" << "\n" << "Release date - " << AMD№2.day << "." << AMD№2.month << "." << AMD№2.year << "\n" << "Prize -" << AMD№2.prize << "$" << endl;
+	}
+	else if (AMD№3.prize < AMD№2.prize && AMD№3.prize < AMD№1.prize) {
+		cout << "Producer -" << " AMD \n" << "Size of RAM - " << AMD№3.size << " Gb" << "\n" << "Release date - " << AMD№3.day << "." << AMD№3.month << "." << AMD№3.year << "\n" << "Prize -" << AMD№3.prize << "$" << endl;
+	}
+	return 0;
 }
