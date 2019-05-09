3da2b29b528662086cdd854a429a8fd956ec3e50
WitcherFromRivia <48452428+WitcherFromRivia@users.noreply.github.com>
2019-05-09 21:02:47 +0300

    Add files via upload

---
+++ C++ Lab5.cpp
@@ -0,0 +1,25 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+	double arr[4] = {12,44,75,6};
+	ofstream output;
+	output.open("test.txt");
+	output << arr[4];
+	output.close();
+
+	ifstream input;
+	input.open("test.txt");
+	double numb1, numb2, numb3, numb4;
+	input >> numb1 >> numb2 >> numb3 >> numb4;
+	cout << arr[4 - 1];
+	input.close();
+
+
+}
+
+
