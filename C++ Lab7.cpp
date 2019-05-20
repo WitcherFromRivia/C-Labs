7df9de20c02a9c1c30f275e572a26b798b4d1d14
WitcherFromRivia <48452428+WitcherFromRivia@users.noreply.github.com>
2019-05-20 16:42:08 +0300

    Add files via upload

---
+++ C++ Lab7.cpp
@@ -0,0 +1,12 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <queue>        
+using namespace std;
+
+int main()
+{
+	deque<double> q{ 5.66, 1745.87, 355.3,347,6777.7,90 };
+	deque<double>::iterator it = std::min_element(q.begin(), q.end());
+	cout << *it << std::endl;
+}
